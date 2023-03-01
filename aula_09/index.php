a40e7aec7235079febc24dcedc2132f46702cab1
melon musk <125475444+JoaomBRosio@users.noreply.github.com>
2023-03-01 16:41:56 -0300

    Add files via upload

---
+++ aula_09/index.php
@@ -0,0 +1,29 @@
+<?php
+//calculos matematicos
+
+/*$var1 = 'João';
+$var2 = 'João';
+
+
+if($var1 == $var2){
+    echo '<div style="widht: 300px;height: 300px; background:red;"></div>';
+}else{
+    echo 'a condição nao bate';
+}
+*/
+
+$var1 = 10 + 5;
+$var2 = '10 + 5';
+
+if($var1 == $var2){
+    echo 'verdade';
+}else if($var1 != $var2){
+    echo 'verdade na condicao else if';
+}else{  
+    echo 'fake'; 
+}
+
+
+
+
+?>
